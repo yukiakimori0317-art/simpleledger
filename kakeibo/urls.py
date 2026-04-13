9cd67b4e3b9294e969a4c8323afa32a351f47a5f
yuki <yuki.akimori.0317@gmaol.com>
2026-04-13 12:50:21 +0900

    add iphone style logout topbar

--- kakeibo/urls.py
+++ kakeibo/urls.py
@@ -29,6 +29,10 @@ urlpatterns = [
         name='login'
     ),
     path('accounts/signup/', views.signup, name='signup'),
-    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path(
+    'accounts/logout/',
+    auth_views.LogoutView.as_view(next_page='kakeibo:login'),
+    name='logout'
+),, auth_views.LogoutView.as_view(), name='logout'),
 ]
 
